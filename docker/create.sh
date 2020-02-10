df866c44eb347a945d74d736c752e8b6b78ba515
Shai Coleman <shai.coleman@cashanalytics.com>
2020-02-10 17:08:34 +0000

    Remove --image

--- docker/create.sh
+++ docker/create.sh
@@ -4,5 +4,5 @@ aws ec2 create-security-group --group-name fargate-web-app --description fargate
 aws ec2 authorize-security-group-ingress --group-name fargate-web-app --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name fargate-web-app --protocol tcp --port 3000 --cidr 0.0.0.0/0
 security_group=$(aws ec2 describe-security-groups --group-name fargate-web-app --query 'SecurityGroups[].{Name:GroupId}' --output=text)
-fargatecli service create --port http:3000 fargate-web-app --security-group-id $security_group --image ../Dockerfile
+fargatecli service create --port http:3000 fargate-web-app --security-group-id $security_group
 ./stop.sh
--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 [[ -f config.sh ]] && source config.sh
 fargatecli service deploy fargate-web-app
+./start.sh
