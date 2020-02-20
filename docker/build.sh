78bb60b9ef1ac1e56f37feecef579baf46a04d9a
Shai Coleman <shai.coleman@cashanalytics.com>
2020-02-20 14:38:55 +0000

    Update environment variables

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 [[ -f config.sh ]] && source config.sh
-docker build --build-arg WEEKLY_ID=$(date +%G-%V) -t fargate-web-app ..
+docker build --build-arg WEEKLY_ID=$WEEKLY_ID -t fargate-web-app ..
--- docker/config.sh
+++ docker/config.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export AWS_PROFILE=cashanalytics-dev
-export AWS_DEFAULT_PROFILE=cashanalytics-dev
-export AWS_REGION=eu-west-1
 export AWS_DEFAULT_REGION=eu-west-1
+export WEEKLY_ID=$(date +%G-%V)
