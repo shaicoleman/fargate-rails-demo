928faef6189d679f75177d4e4794787f806f9dbd
Shai Coleman <shai.coleman@cashanalytics.com>
2020-02-20 23:32:05 +0000

    Use Docker BuildKit

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 [[ -f config.sh ]] && source config.sh
-docker build --build-arg WEEKLY_ID=$WEEKLY_ID -t fargate-web-app ..
+DOCKER_BUILDKIT=1 docker build --build-arg WEEKLY_ID=$WEEKLY_ID -t fargate-web-app ..
