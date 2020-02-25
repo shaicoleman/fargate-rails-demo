007e17901a6f4e1dfce0adb14d1ac4b3ebe23b50
Shai Coleman <shai.coleman@cashanalytics.com>
2020-02-25 12:27:10 +0000

    Use UTC date

--- docker/config.sh
+++ docker/config.sh
@@ -2,7 +2,7 @@
 export AWS_PROFILE=cashanalytics-dev
 export AWS_DEFAULT_REGION=eu-west-1
 
-export WEEKLY_ID=$(date +%G-%V)
+export WEEKLY_ID=$(date --utc +%G-%V)
 export RUBY_VERSION=$(<../.ruby-version)
 export NODE_VERSION=$(<../.node-version)
 export BUNDLER_VERSION=$(grep -oPz 'BUNDLED WITH\n\s+\K\S+' ../Gemfile.lock | tr -d '\000')
